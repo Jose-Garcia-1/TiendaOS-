df0275943448c9de1bd6f9ad3f63fd2e1e2be31a
Jose-Garcia-1 <130235766+Jose-Garcia-1@users.noreply.github.com>
2026-07-10 17:04:49 -0500

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Cierre de sesión del usuario.
-   Destruye la sesión activa y redirige al usuario a la página de inicio de sesión. */
-session_start();
-session_destroy();
-header("Location: login.php");
-exit;
